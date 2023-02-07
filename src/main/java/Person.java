48a963a677b958c4e00f7c7c08ade53125461572
StoefflerMario <stoeffler@mario.at>
2023-02-07 17:56:20 +0100

    new dependency added;

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+
+    private String name;
+    private int alter;
+    private String geschlecht;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public String getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setGeschlecht(String geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+
+    public void alterErhoehen() {
+        alter = alter +1;
+    }
+
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,4 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+
+    Person person = new Person();
+
+    @BeforeEach
+    void setUp() {
+        person.setName("Manfred");
+        person.setGeschlecht("männlich");
+        person.setAlter(34);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void alterErhoehen() {
+        int excpected = 35;
+        person.alterErhoehen();
+        assertEquals(excpected, person.getAlter());
+    }
 }
